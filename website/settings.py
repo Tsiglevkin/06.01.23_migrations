28ab19087e3a7863e8f36d2ad541c95ddddf8ceb
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-01-09 23:07:11 +0500

    Change README.md, change students_list.html, add path in urls.py, add function in views.py.

--- website/settings.py
+++ website/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'rozibu1991',
+        'PASSWORD': '',  # введите свой пароль
 
     }
 }
