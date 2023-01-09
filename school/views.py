c2a147f2220c5dc54a01b1639483ec232ae093ac
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-01-09 23:07:11 +0500

    Change README.md, change students_list.html, add path in urls.py, add function in views.py.

--- school/views.py
+++ school/views.py
@@ -2,16 +2,33 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
+    """Функция возвращает рендер со списком студентов, упорядоченных по группе"""
     template = 'school/students_list.html'
     ordering = 'group'  # название столбца таблицы
-    persons = Student.objects.order_by(ordering)
+    persons = Student.objects.order_by(ordering).prefetch_related('teachers')
     context = {
         'object_list': persons
     }
 
     return render(request, template, context)
+
+
+def add_teachers(request):
+    """Функция принимает request и добавляет каждому ученику всех доступных учителей"""
+    students = Student.objects.all()
+    teachers = Teacher.objects.all()
+
+    for student in students:  # добавляем каждого учителя к каждому студенту.
+        for teacher in teachers:
+            student.teachers.add(teacher)
+
+    return HttpResponse('Каждому ученику добавлены все учителя.')
+
+
+
+
 
